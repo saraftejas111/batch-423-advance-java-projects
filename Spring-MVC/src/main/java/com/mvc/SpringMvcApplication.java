b021487b29ac8a66061df297f899c61e8bd89594
Tejas Saraf <tejas@thekiranacademy.com>
2026-04-16 13:18:51 +0530

    mvc

---
+++ Spring-MVC/src/main/java/com/mvc/SpringMvcApplication.java
@@ -0,0 +1,14 @@
+package com.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcApplication.class, args);
+		System.err.println("app started...");
+	}
+
+}
---
+++ Spring-MVC/src/main/webapp/Hello.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+
+<h1 align="center">
+Hello Welcome to my 1st JSP Page
+</h1>
+</body>
+</html>
